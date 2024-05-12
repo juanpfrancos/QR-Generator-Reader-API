58de93eceab4a65609ea1d485a644afbdb7b32da
Juan Pablo <juanpanime@gmail.com>
2024-05-12 16:03:59 -0500

    Disable CORS for developmen

--- app/main.py
+++ app/main.py
@@ -4,9 +4,23 @@ from io import BytesIO
 from fastapi import FastAPI, Header, HTTPException
 from PIL import Image
 import qrcode
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
